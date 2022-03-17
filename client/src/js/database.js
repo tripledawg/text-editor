21bb8f15a8ee52086067942f05000d23e5ce7738
tripledawg <iamlesliecrouch>
2022-03-17 12:14:18 -0400

    added put and get route logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,3 +1,4 @@
+import { request } from 'express';
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -13,9 +14,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+const initDB = await openDB('jate', 1);
+const text = jateDB.transaction('jate', 'readwrite');
+const memory = tx.objectStore('jate');
+const update = store.put({id:1, value: content});
+const newtxt = await request; 
+};
+//console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const initDB = await openDB('jate', 1);
+const text = jateDB.transaction('jate', 'readonly');
+const memory = tx.objectStore('jate');
+const getreq = store.get(1);
+const newtxt = await request; 
+}
+//console.error('getDb not implemented');
 
 initdb();
