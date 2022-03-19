ca35f38bb748f63b0bc9d12b0cbf4a9e18255ac9
tripledawg <iamlesliecrouch>
2022-03-19 14:19:12 -0400

    database.js const name changes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,4 +1,3 @@
-import { request } from 'express';
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -13,28 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Method that takes some content and adds it to the IndexedDB database using the idb module
 export const putDb = async (content) => {
-  console.log('ready to PUT')
-const initDB = await openDB('jate', 1);
-const text = jateDB.transaction('jate', 'readwrite');
-const memory = tx.objectStore('jate');
-const update = store.put({id:1, value: content});
-const newtxt = await request; 
-console.log('data saved', result.value);
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result.value);
 };
-//console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
+// Method that gets content from the IndexedDB database using the idb module
 export const getDb = async () => {
-  console.log('ready to GET')
-  const initDB = await openDB('jate', 1);
-const text = jateDB.transaction('jate', 'readonly');
-const memory = tx.objectStore('jate');
-const getreq = store.get(1);
-const newtxt = await request; 
-result ? console.log ('data pulled from database', result.value) : console.log('no data found in the database')
-}
-//console.error('getDb not implemented');
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
+  return result?.value;
+};
 
 initdb();
